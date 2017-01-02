0000addd07fe76ac4bc1005fcdee4104923c62aa
DarkMatter <dark matter>
2017-01-01 20:14:05 -0800

    Adding base class for Duck

---
+++ Duck.java
@@ -0,0 +1,9 @@
+/* Toy test implementation to get familiar with Design Patterns
+ */
+public abstract class Duck {
+    public Duck () {
+    }
+
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+}
